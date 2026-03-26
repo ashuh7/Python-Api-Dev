0e4300bd974eab2ce4b13e9acd4ab9b7e647f484
AshutoshH <hagawaneashutosh1@gmail.com>
2026-03-26 21:27:58 +0530

    Added FastAPI setup with GET path operation and Postman API testing

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to my api"}
+    return {"message": "Wooo Ye le laude...."}
+
+# New path operation : Retrieving the bunch of social media posts from our application
+@app.get("/posts")
+def get_posts():
+    return {"Data": "THis is your posts"}
+
+# New path operation : Post the bunch of social media posts to our application
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
